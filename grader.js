65a81ba684e2ffe7ad3c07f40fa035ac5c0fa84c
Maciej Swinarski <maciej.swinarski@gmail.com>
2013-07-12 07:16:00 +0000

    grader.js done

--- grader.js
+++ grader.js
@@ -23,6 +23,7 @@ References:
 
 var fs = require('fs');
 var program = require('commander');
+var rest = require('restler');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
@@ -36,16 +37,22 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
-var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
+var assertUrl = function(url) {
+    //console.log("url: %s",url);
+    return url;
+}
+
+var cheerioHtmlFile = function(html) {
+    return cheerio.load(html);
 };
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkHtmlFile = function(html, checksfile) {
+    //console.log("html: %s", html);
+    $ = cheerioHtmlFile(html);
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -65,10 +72,26 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'URL to index.html', assertUrl)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+   
+    //console.log("url parameter: %s", program.url);
+
+    var p = function(html, checks) {
+      var checkJson = checkHtmlFile(html, checks);
+      var outJson = JSON.stringify(checkJson, null, 4);
+      console.log(outJson);
+    };
+
+    if (program.url) {
+      //console.log("url before rest: %s", program.url);
+      var handle = function (result, response) {
+        p(result, program.checks);
+      };
+      rest.get(program.url).on('complete', handle);
+    } else {
+      p(fs.readFileSync(program.file), program.checks);
+    }  
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
